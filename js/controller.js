2c912ad97a3fcdfef221a79b6bff60e92d887cd8
sitambas <sitambas@gmail.com>
2016-02-29 09:03:42 +0530

    angular file update

---
+++ js/controller.js
@@ -0,0 +1,24 @@
+/*** Main AngularJS Web Application */
+var askme = angular.module('askMeApp', ['ngRoute'])
+
+/*** Configure the Routes */
+askme.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "build/category.html",
+            controller: "couponcatCtrl"
+        })
+}]);
+
+askme.controller('couponcatCtrl', function($scope, $location, $http) {
+    $http.get('json/askme.json').success(function(response) {
+        $scope.categoryFilter   =   response.filter.category;
+        $scope.colorFilter      =   response.filter.color;
+        $scope.sizeFilter       =   response.filter.size;
+    });
+        jQuery('.checklist li').click(function(){
+            jQuery(this).find('span.x').addClass('animatex');
+            jQuery(this).find('span.y').addClass('animatey');
+        });
+
+});
